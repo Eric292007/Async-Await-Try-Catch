628d10eb61446c0452a702d2d9661fb733aedaf8
Eric_V <110581181+Eric292007@users.noreply.github.com>
2026-09-17 19:35:06 +0700

    Versi Sekian

--- index.dart
+++ index.dart
@@ -31,10 +31,33 @@ void main() {
   //Tanpa nilai balik, pakai void(tidak ada pengembalian nilai)
 
   //tanpa nilai balik
-  void hitung(int a1, int a2, int a3) {
-    num hasil = a1 + a2 + a3;
-    print('Ini hasil $hasil');
-  }
+  // void hitung(int? a1, int? a2, int? a3) {
+  //   num hasil = (a1 ?? 0) + (a2 ?? 0) + (a3 ?? 0);
+  //   print('Ini hasil $hasil');
+  // }
 
-  hitung(100, 200, 300);
+  // hitung(100, 200, 300);
+  // hitung(100, 200, null);
+  // hitung(100, null, null);
+  // hitung(null, null, null);
+
+  //Fungsi ada nilai balik
+
+  // String cetaknilailengkap(String nd, String? nb) {
+  //   var namalengkap = nd + ' ' + (nb ?? '');
+  //   return namalengkap;
+  // }
+
+  // String nama = cetaknilailengkap('Wendy', 'Wen');
+  // String nama1 = cetaknilailengkap('Wendy', null);
+  // print(nama);
+  // print(nama1);
+
+  // num hitung(int a1, int a2, int a3) {
+  //   num hasil = a1 + a2 + a3;
+  //   return hasil;
+  // }
+
+  // num nilai = hitung(100, 200, 300);
+  // print(nilai);
 }
