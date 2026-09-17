3a0f7bdc9952b0fa7917054c84da1edf970f7178
Eric_V <110581181+Eric292007@users.noreply.github.com>
2026-09-17 19:12:27 +0700

    Versi Sekian

--- index.dart
+++ index.dart
@@ -1,3 +1,40 @@
+import 'dart:io';
+
 void main() {
-  
+  //!
+  // stdout.writeln('Masukkan angka 1 : ');
+  // var input1 = stdin.readLineSync()!;
+  // stdout.writeln('Masukkan angka 2 : ');
+  // var input2 = stdin.readLineSync()!;
+
+  // num hasil = int.parse(input1) + int.parse(input2);
+
+  // print(hasil);
+
+  //?
+  // String? namadepan;
+  // String? namabelakang;
+
+  // namadepan = 'Wendy';
+  // namabelakang = 'Wen';
+
+  // String namalengkap = namadepan + namabelakang;
+
+  // String? nama;
+  // nama = 'Wendy';
+  // print(nama);
+
+  //??
+
+  //Function 2 jenis
+  //Ada nilai balik
+  //Tanpa nilai balik, pakai void(tidak ada pengembalian nilai)
+
+  //tanpa nilai balik
+  void hitung(int a1, int a2, int a3) {
+    num hasil = a1 + a2 + a3;
+    print('Ini hasil $hasil');
+  }
+
+  hitung(100, 200, 300);
 }
